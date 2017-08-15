76a45c943dc27cf3e445cffd917590bb23f00651
rxmeez <rameezkhan.1@hotmail.co.uk>
2017-08-15 02:17:45 +0100

    Convolution, MaxPooling, Activation Layers added. This improve the previous models by miles, however it still struggled around corners even after I vertical flipped images, and corrected steering wheel.

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -17,9 +19,13 @@ for line in lines:
     filename = source_path.split('/')[-1]
     current_path = './data/IMG/' + filename
     image = cv2.imread(current_path)
+    vertical_flip_image = cv2.flip(image, 1)
     images.append(image)
+    images.append(vertical_flip_image)
     measurement = float(line[3])
+    vertical_measurement_flip = float(line[3]) * -1.0
     measurements.append(measurement)
+    measurements.append(vertical_measurement_flip)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -27,7 +33,13 @@ y_train = np.array(measurements)
 # print(X_train.shape, y_train.shape)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 5, 5, activation='elu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(6, 5, 5, activation='elu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Activation('elu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
